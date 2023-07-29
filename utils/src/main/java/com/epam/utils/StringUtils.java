3913a83b27c75f8035b6a055bddb25e08e80d64e
Ivan Artsiushyn <ivanart555@gmail.com>
2023-07-29 13:49:46 +0300

    Solve tasks

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,13 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        if (NumberUtils.isCreatable(str)) {
+            double number = NumberUtils.toDouble(str, Double.NaN);
+            return number > 0.0 && !Double.isNaN(number);
+        } else return false;
+
     }
 }
